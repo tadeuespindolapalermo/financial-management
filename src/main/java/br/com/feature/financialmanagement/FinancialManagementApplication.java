74bce7c08c4995c03a40a24482036096c50776d9
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2021-04-26 21:16:50 -0300

    Initial commit

---
+++ src/main/java/br/com/feature/financialmanagement/FinancialManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.feature.financialmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/feature/financialmanagement/FinancialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feature.financialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
